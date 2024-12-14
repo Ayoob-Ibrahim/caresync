694155a6eff9b946ea007c24c9b3ec0efb90bd24
ayoob-ibrahim <ayoobibrahim31@gmail.com>
2024-12-14 12:41:34 +0530

    router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,13 @@ import { LayoutComponent } from './structure-layout/layout/layout.component';
 export const routes: Routes = [
     {
         path: '', component: LayoutComponent,
-         
+        children: [
+            {
+                path: 'home',
+                loadComponent: () =>
+                    import('./page/home/home.component').then((m) => m.HomeComponent),
+            },
+        ]
+
     },
 ];
