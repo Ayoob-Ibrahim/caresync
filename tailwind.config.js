f65eeb2ddd0a1f5611a192220e9cab6dfd21c0a2
riyazapm <riyas@apmkingstrack.com>
2024-12-13 23:59:51 -0800

    notes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,20 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'kanitb': ['Kanit-bold']
+        'kanitb': ['Kanit-bold'],
+        'lightkanit':['EKanit'],
+        'semikanit':['semikanit'],
+        'regular':['regular']
       }
     },
+    colors:{
+      primary: '#129693',
+      backgroundcolor:' #004558',
+      toolbarprimary: '#667085',
+      footer: '#98A2B3',
+      normaltext: '#323433',
+      whitenormal: '#ffffff',
+    }
     
   },
   plugins: [],
