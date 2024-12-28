8e0c5d943834940dfdd4bb69033c3ce2de4754f5
ayoob-ibrahim <ayoobibrahim31@gmail.com>
2024-12-28 17:44:24 +0530

    zxczxc

--- src/app/component/home-page-entry-carousel/home-page-entry-carousel.component.ts
+++ src/app/component/home-page-entry-carousel/home-page-entry-carousel.component.ts
@@ -15,46 +15,46 @@ export class HomePageEntryCarouselComponent {
   constructor(private cdRef: ChangeDetectorRef) {}
 
   ngAfterViewInit() {
-    // new Swiper('.swiper-container', {
-    //   slidesPerView: 1,
-    //   spaceBetween: 10,
-    //   effect: 'fade',
-    //   speed: 800,
-    //   loop: true,
-    //   grabCursor: true,
-    //   navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    //   },
-    //   pagination: {
-    //     el: '.swiper-pagination',
-    //     clickable: true,
-    //   },
-    //   touchRatio: 1,
-    // });
-    // this.cdRef.detectChanges();
-    // new Swiper('.swiper-container', {
-    //   modules: [Autoplay, Navigation, Pagination, EffectFade],
-    //   slidesPerView: 1,
-    //   spaceBetween: 10,
-    //   effect: 'fade',
-    //   speed: 800,
-    //   loop: true,
-    //   grabCursor: true,
-    //   navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    //   },
-    //   pagination: {
-    //     el: '.swiper-pagination',
-    //     clickable: true,
-    //   },
-    //   autoplay: {
-    //     delay: 3000,
-    //     disableOnInteraction: false,
-    //   },
-    //   touchRatio: 1,
-    // });
-    // this.cdRef.detectChanges();
+    new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      effect: 'fade',
+      speed: 800,
+      loop: true,
+      grabCursor: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      touchRatio: 1,
+    });
+    this.cdRef.detectChanges();
+    new Swiper('.swiper-container', {
+      modules: [Autoplay, Navigation, Pagination, EffectFade],
+      slidesPerView: 1,
+      spaceBetween: 10,
+      effect: 'fade',
+      speed: 800,
+      loop: true,
+      grabCursor: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      touchRatio: 1,
+    });
+    this.cdRef.detectChanges();
   }
 }
