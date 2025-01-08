3f85127ba592c908df613333311ca71725e7b31c
ayoob-ibrahim <ayoobibrahim31@gmail.com>
2025-01-08 08:20:55 +0530

    chnages in packages

--- src/app/page/packages-menu/packages-menu.component.ts
+++ src/app/page/packages-menu/packages-menu.component.ts
@@ -1,23 +1,7 @@
-import {
-  Component,
-  computed,
-  effect,
-  inject,
-  OnInit,
-  signal,
-} from '@angular/core';
+import { Component, computed, effect, inject, OnInit, signal, } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { list_comp } from './packages-widgets-imports';
-import { FullImageWithCardBottomComponent } from '../../widgets/full-image-with-card-bottom/full-image-with-card-bottom.component';
 import { HttpService } from '../../service/http.service';
-import { OurpackagesAccorianGroupComponent } from '../../component/ourpackages-accorian-group/ourpackages-accorian-group.component';
-import { ImageCardDataComponent } from '../../widgets/image-card-data/image-card-data.component';
-import { ThoroughSupportforCqcRegistrationComponent } from '../../widgets/thorough-supportfor-cqc-registration/thorough-supportfor-cqc-registration.component';
-import { CyanWhiteCardComponent } from '../../widgets/cyan-white-card/cyan-white-card.component';
-import { PointersWithgridcardComponent } from '../../widgets/pointers-withgridcard/pointers-withgridcard.component';
-import { ArrowPointsAndBannerComponent } from '../../widgets/arrow-points-and-banner/arrow-points-and-banner.component';
-import { RoundedArrowsContentComponent } from '../../widgets/rounded-arrows-content/rounded-arrows-content.component';
-import { ThreeCardRowGridComponent } from '../../widgets/three-card-row-grid/three-card-row-grid.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -41,7 +25,6 @@ export class PackagesMenuComponent implements OnInit {
       if (id) {
         const data = this.serviceData.GetPackagesData()[id];
         this.customInjector.set(data);
-        console.log(this.customInjector());
       }
     });
   }
