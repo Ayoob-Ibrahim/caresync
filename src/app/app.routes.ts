dbd609abe6e17d43a2c1ded94bc71a50a47d9c66
ayoob-ibrahim <ayoobibrahim31@gmail.com>
2025-01-14 11:00:57 +0530

    cxccxzc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './structure-layout/layout/layout.component';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home', },
@@ -17,9 +19,18 @@ export const routes: Routes = [
                     import('./page/aboutus/aboutus.component').then((m) => m.AboutusComponent),
             },
             {
-                path: 'packages/:id', 
-                loadComponent: () =>
-                    import('./page/packages-menu/packages-menu.component').then((m) => m.PackagesMenuComponent),
+                path: 'packages/:id',
+                loadComponent: () => {
+                    const platformId = inject(PLATFORM_ID);
+                    if (isPlatformBrowser(platformId)) {
+                        return import(
+                            './page/packages-menu/packages-menu.component'
+                        ).then((m) => m.PackagesMenuComponent);
+                    } else {
+                        return Promise.resolve(null);
+                    }
+                },
+                canActivate: [false],
             }
         ]
 
