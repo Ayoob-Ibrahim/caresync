d98af8b73d5454f820b659c7d57af92d15369a1f
ayoob-ibrahim <ayoobibrahim31@gmail.com>
2025-01-14 11:05:30 +0530

    asas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,17 +20,13 @@ export const routes: Routes = [
             },
             {
                 path: 'packages/:id',
-                loadComponent: () => {
-                    const platformId = inject(PLATFORM_ID);
-                    if (isPlatformBrowser(platformId)) {
-                        return import(
-                            './page/packages-menu/packages-menu.component'
-                        ).then((m) => m.PackagesMenuComponent);
-                    } else {
-                        return Promise.resolve(null);
-                    }
-                },
-                canActivate: [false],
+                loadComponent: () =>
+                    import(
+                        './page/packages-menu/packages-menu.component'
+                    ).then((m) => m.PackagesMenuComponent)
+
+                ,
+
             }
         ]
 
