1facb10748ddc4098663a1b8cf83fb23b85db4fb
ayoob-ibrahim <ayoobibrahim31@gmail.com>
2025-01-16 19:54:27 +0530

    buisnees etup menu over

--- src/app/page/serivces-menu/service-widgets.ts
+++ src/app/page/serivces-menu/service-widgets.ts
@@ -1,9 +1,15 @@
 import { ListVerticalCardsInServiceComponent } from "../../service-widgets/list-vertical-cards-in-service/list-vertical-cards-in-service.component";
 import { ServiceInitialPageWithImageComponent } from "../../service-widgets/service-initial-page-with-image/service-initial-page-with-image.component";
+import { ThreeRowContentComponent } from "../../service-widgets/three-row-content/three-row-content.component";
+import { FreequentlyAskedComponent } from "../../widgets/freequently-asked/freequently-asked.component";
+import { HorizontalCardComponent } from "../../widgets/horizontal-card/horizontal-card.component";
 
 export const list_service_comp = {
     'business-setup': [
         ServiceInitialPageWithImageComponent,
-        ListVerticalCardsInServiceComponent
+        ListVerticalCardsInServiceComponent,
+        ThreeRowContentComponent,
+        FreequentlyAskedComponent,
+        HorizontalCardComponent
     ]
 }
--- src/app/service-widgets/three-row-content/three-row-content.component.ts
+++ src/app/service-widgets/three-row-content/three-row-content.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { BaseDataReciever } from '../../baseclass/parent-data-reciever';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-three-row-content',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './three-row-content.component.html',
   styleUrl: './three-row-content.component.scss'
 })
-export class ThreeRowContentComponent {
+export class ThreeRowContentComponent extends BaseDataReciever {
 
 }
