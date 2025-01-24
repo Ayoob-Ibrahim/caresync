50dc43b1b74c4ab009757ff04248675b2651fa8b
ayoob-ibrahim <ayoobibrahim31@gmail.com>
2025-01-24 09:51:51 +0530

    merge changes

--- src/app/page/serivces-menu/service-widgets.ts
+++ src/app/page/serivces-menu/service-widgets.ts
@@ -11,27 +11,64 @@ export const list_service_comp = {
   'cqc-registration': [
     BgArrowUpComponent,
     StepCardComponent,
-    ClientCardComponent,
+    ListVerticalCardsInServiceComponent,
+    // ClientCardComponent,
+  ],
+  'marketing-support': [
+    BgArrowUpComponent,
+    StepCardComponent,
+    ListVerticalCardsInServiceComponent,
+    // ClientCardComponent,
+  ],
+  'comprehensive-training': [
+    BgArrowUpComponent,
+    StepCardComponent,
+    ListVerticalCardsInServiceComponent,
+    // ClientCardComponent,
+  ],
+  'tender-writing': [
+    BgArrowUpComponent,
+    StepCardComponent,
+    ListVerticalCardsInServiceComponent,
+    // ClientCardComponent,
+  ],
+  'ofsted-related-services': [
+    BgArrowUpComponent,
+    StepCardComponent,
+    ListVerticalCardsInServiceComponent,
+    // ClientCardComponent,
+  ],
+  'pir-writing': [
+    BgArrowUpComponent,
+    StepCardComponent,
+    ListVerticalCardsInServiceComponent,
+    //  ClientCardComponent
+  ],
+  'compliance-management': [
+    BgArrowUpComponent,
+    StepCardComponent,
+    ListVerticalCardsInServiceComponent,
+    // ClientCardComponent,
   ],
   'business-setup': [
     ServiceInitialPageWithImageComponent,
     ListVerticalCardsInServiceComponent,
     ThreeRowContentComponent,
     FreequentlyAskedComponent,
-    HorizontalCardComponent,
+    HorizontalCardComponent, HorizontalCardComponent
   ],
   'client-acquisition': [
     ServiceInitialPageWithImageComponent,
     ListVerticalCardsInServiceComponent,
     ThreeRowContentComponent,
     FreequentlyAskedComponent,
-    HorizontalCardComponent,
+    HorizontalCardComponent, HorizontalCardComponent
   ],
   branding: [
     ServiceInitialPageWithImageComponent,
     ListVerticalCardsInServiceComponent,
     ThreeRowContentComponent,
     FreequentlyAskedComponent,
-    HorizontalCardComponent,
+    HorizontalCardComponent, HorizontalCardComponent
   ],
 };
