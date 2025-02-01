59790936a00fe0d568813094d62b27d06cd1cbae
ayoob-ibrahim <ayoobibrahim31@gmail.com>
2025-02-01 21:06:06 +0530

    arrow five arrow card

--- src/app/widgets/image-card-data/image-card-data.component.ts
+++ src/app/widgets/image-card-data/image-card-data.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { BaseDataReciever } from '../../baseclass/parent-data-reciever';
+import { SvgComponent } from '../../component/svg/svg.component';
 
 @Component({
   selector: 'app-image-card-data',
-  imports: [CommonModule],
+  imports: [CommonModule, SvgComponent],
   templateUrl: './image-card-data.component.html',
   styleUrl: './image-card-data.component.scss'
 })
