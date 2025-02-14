1a97d447947fa49989d8ed1e37a71d6ecfd393fa
ayoob-ibrahim <ayoobibrahim31@gmail.com>
2025-02-14 21:52:12 +0530

    sas

---
+++ src/app/elevate-care-widget/lite-blue-panel/lite-blue-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lite-blue-panel',
+  imports: [],
+  templateUrl: './lite-blue-panel.component.html',
+  styleUrl: './lite-blue-panel.component.scss'
+})
+export class LiteBluePanelComponent {
+
+}
