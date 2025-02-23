2ca1466785f0107c2ce3773e51c578248b459501
ayoob-ibrahim <ayoobibrahim31@gmail.com>
2025-02-23 10:17:50 +0530

    brouchers comleted

--- src/app/page/resource/brouchers/brouchers.component.ts
+++ src/app/page/resource/brouchers/brouchers.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import resources from '../../../json-data/resources.json'
+import { HorizontalCardComponent } from "../../../widgets/horizontal-card/horizontal-card.component";
 @Component({
   selector: 'app-brouchers',
-  imports: [],
+  imports: [CommonModule, HorizontalCardComponent],
   templateUrl: './brouchers.component.html',
   styleUrl: './brouchers.component.scss'
 })
 export class BrouchersComponent {
-
+  brouchers = resources;
 }
