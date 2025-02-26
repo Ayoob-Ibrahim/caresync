97fe9d5f66697e63f923cf18c1cc7810df29d62b
ayoob-ibrahim <ayoobibrahim31@gmail.com>
2025-02-26 20:57:27 +0530

    changes e book inital

--- src/app/page/resource/e-books-home/e-book-initial-page/e-book-initial-page.component.ts
+++ src/app/page/resource/e-books-home/e-book-initial-page/e-book-initial-page.component.ts
@@ -1,16 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
-
+import resources from '../../../../json-data/resources.json'
+import { HorizontalCardComponent } from "../../../../widgets/horizontal-card/horizontal-card.component";
 @Component({
   selector: 'app-e-book-initial-page',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule, HorizontalCardComponent],
   templateUrl: './e-book-initial-page.component.html',
   styleUrl: './e-book-initial-page.component.scss'
 })
 export class EBookInitialPageComponent {
+  brouchers = resources;
 
 
-  
 
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
