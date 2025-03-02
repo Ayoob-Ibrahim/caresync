51a4a06df8cbb73442fcbc9392441e5a6e6e6904
ayoob-ibrahim <ayoobibrahim31@gmail.com>
2025-03-02 14:19:20 +0530

    ebook basic wokr is done

--- src/app/page/resource/e-books-home/e-book-detailed-view/e-book-detailed-view.component.ts
+++ src/app/page/resource/e-books-home/e-book-detailed-view/e-book-detailed-view.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-e-book-detailed-view',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './e-book-detailed-view.component.html',
   styleUrl: './e-book-detailed-view.component.scss'
 })
 export class EBookDetailedViewComponent {
-
+  formJson = [
+    { formName: 'Your name/Company’s Name', icon: 'bi bi-person-lines-fill' },
+    { formName: 'Email address', icon: 'bi bi-envelope' },
+    { formName: 'Contact number', icon: 'bi bi-phone-vibrate' },
+    { formName: 'Company’s address', icon: 'bi bi-geo-alt-fill' },
+    { formName: 'How many care recipients (clients) do you have?', icon: 'bi bi-people-fill' },
+  ]
 }
